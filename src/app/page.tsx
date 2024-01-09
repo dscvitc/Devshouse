01dc608d02da379324da9ef9e1024f44d957819f
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2024-01-09 19:04:38 +0530

    fix hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ import Image from 'next/image';
   export default function Home() {
     return <>
       <HeroBullets />
-      {/* <Tracks />
+       <Tracks />
       <Ourteam/>
-      <Contact /> */}
+      <Contact /> 
     </>
 }
 
