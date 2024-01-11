8cd4c8828a6aac02c07fbef66c66d3db2f2deda5
abhi <abhinavkush2003@gmail.com>
2024-01-11 10:06:09 +0530

    about page responsiveness fixed - abhi

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -20,21 +20,12 @@ const AboutPage: React.FC = () => {
                     color: '#fff',
                     overflowX: 'hidden',
                     overflowY: 'hidden',
-
-                    //Responsive styles
-                    // Media queries
-                    ...(typeof window !== 'undefined' && window.matchMedia('(max-width: 768px)').matches
-                        ? {
-                            backgroundPosition: 'center bottom, center top',
-                        }
-                        : {}),
-                    ...(typeof window !== 'undefined' && window.matchMedia('(max-width: 480px)').matches
-                        ? {
-                            backgroundPosition: 'center bottom, center top',
-                            backgroundSize: 'cover',
-                        }
-                        : {}),
                 }}>
+
+
+
+
+                {/* --------------------------------------------------------------------------------------------*/}
                 <div style={{
                     display: 'flex',
                     flexDirection: 'row',
@@ -63,109 +54,145 @@ const AboutPage: React.FC = () => {
                             }}>bout the<br /> Community</div>
                 </div>
 
+                {/* --------------------------------------------------------------------------------------------*/}
+
+
+
                 <div
-                    className={Space.className}
                     style={{
-                        height: '80%',
                         display: 'grid',
                         gridTemplateColumns: 'repeat(2, 50%)',
-                        gridTemplateRows: 'repeat(2, 50%)'
+
                     }}>
 
-                    <div style={{
-                        marginLeft: '10%',
-                        marginRight: '5%'
-                    }}>
-                        <h3 style={{
-                            fontSize: '24px',
-                            fontWeight: '700',
-                            lineHeight: '31px',
-                            letterSpacing: '0em',
-                            textAlign: 'left'
-                        }}>ABOUT</h3>
-                        <h2 style={{
-                            fontSize: '40px',
-                            fontWeight: '700',
-                            lineHeight: '55px',
-                            letterSpacing: '0em',
-                            textAlign: 'left',
-                            marginTop: '-5px'
-                        }}>VIT Chennai</h2>
-                        <p
-                            className={Work.className}
-                            style={{
-                                fontSize: '20px',
-                                fontWeight: '400',
-                                lineHeight: '25px',
+
+                    <div
+                        className={Space.className}
+                        style={{
+                            display: 'grid',
+                            gridTemplateRows: '60% 30%'
+                        }}>
+
+                        <div style={{
+                            marginLeft: '10%',
+                            marginRight: '5%'
+                        }}>
+                            <h3 style={{
+                                fontSize: '1.3em',
+                                fontWeight: '700',
+                                lineHeight: '31px',
                                 letterSpacing: '0em',
-                                marginTop: '25px',
-                                textAlign: 'left',
-                                color: '#787878'
-                            }}>Vellore Institute of Technology, founded in 1984 is one of the leading engineering colleges in India. It aims to provide a world-class education that not only polishes technical mastery and analytical skills but also imbibes strong ethical values in their students.</p>
-                    </div>
-                    <div>
-                        <Image alt="college" style={{
-
-                            ...(typeof window !== 'undefined' && window.matchMedia('(max-width: 580px)').matches
-                                ? {
-                                    marginTop: '-122%',
-                                }
-                                : {}),
-                            ...(typeof window !== 'undefined' && window.matchMedia('(max-width: 480px)').matches
-                                ? {
-                                    marginTop: '-122%',
-
-                                }
-                                : {}),
-                            height: '180%',
-                            marginLeft: '-12%',
-                            marginTop: '-22%',
-                        }} src={col_img} />
-                    </div >
-                    <div>
-                        <Image alt="gdsc" style={{
-                            height: '105%',
-                            marginLeft: '-4%',
-                            marginTop: '-5%'
-                        }} src={gdsc_img} />
-                    </div >
-
-                    <div style={{
-                        marginLeft: '0%',
-                        marginRight: '10%'
-                    }}>
-                        <h3 style={{
-
-                            fontSize: '24px',
-                            fontWeight: '700',
-                            lineHeight: '31px',
-                            letterSpacing: '0em',
-                            textAlign: 'left'
-                        }}>ABOUT</h3>
-                        <h2
-                            className={Space.className}
-                            style={{
-                                fontSize: '40px',
+                                textAlign: 'left'
+                            }}>ABOUT</h3>
+                            <h2 style={{
+                                fontSize: '2.5em',
                                 fontWeight: '700',
                                 lineHeight: '55px',
                                 letterSpacing: '0em',
                                 textAlign: 'left',
-                                marginTop: '-5px'
-                            }}> Google Developer Student Club</h2>
-                        <p
-                            className={Work.className}
-                            style={{
-                                fontSize: '20px',
-                                fontWeight: '400',
-                                lineHeight: '25px',
+                                marginTop: '-5px',
+                                ...(typeof window !== 'undefined' && window.matchMedia('(max-width: 580px)').matches
+                                    ? {
+                                        fontSize: '2em',
+                                        lineHeight: '35px',
+                                    }
+                                    : {}),
+                            }}>VIT Chennai</h2>
+                            <p
+                                className={Work.className}
+                                style={{
+                                    fontSize: '1.3em',
+                                    fontWeight: '400',
+                                    lineHeight: '25px',
+                                    letterSpacing: '0em',
+                                    marginTop: '15px',
+                                    textAlign: 'left',
+                                    color: '#787878',
+                                    ...(typeof window !== 'undefined' && window.matchMedia('(max-width: 580px)').matches
+                                        ? {
+                                            fontSize: '1em',
+                                            lineHeight: '20px',
+                                        }
+                                        : {}),
+                                }}>Vellore Institute of Technology, founded in 1984 is one of the leading engineering colleges in India. It aims to provide a world-class education that not only polishes technical mastery and analytical skills but also imbibes strong ethical values in their students.</p>
+                        </div>
+                        <div>
+                            <Image alt="gdsc"
+                                style={{
+                                    height: '100%',
+                                }} src={gdsc_img} />
+                        </div >
+
+                    </div>
+
+
+
+                    <div style={{
+                        display: 'grid',
+                        gridTemplateRows: '35% 65%'
+                    }}>
+                        <div>
+                            <Image alt="college" style={{
+                                height: '100%',
+
+                            }} src={col_img} />
+                        </div >
+
+
+                        <div style={{
+                            marginLeft: '0%',
+                            marginRight: '10%',
+
+
+                        }}>
+                            <h3 style={{
+
+                                fontSize: '1.3em',
+                                fontWeight: '700',
+                                lineHeight: '31px',
                                 letterSpacing: '0em',
-                                marginTop: '25px',
                                 textAlign: 'left',
-                                color: '#787878'
-                            }}>GDSC VIT Chennai is a student run community group powered by Google Developers. We are a passionate group of budding developers who work together to build solutions for local businesses and communities with Google technology and promote healthy developer culture.</p>
-                    </div >
+
+
+                            }}>ABOUT</h3>
+                            <h2
+                                className={Space.className}
+                                style={{
+                                    fontSize: '2.5em',
+                                    fontWeight: '700',
+                                    lineHeight: '55px',
+                                    letterSpacing: '0em',
+                                    textAlign: 'left',
+                                    marginTop: '-5px',
+                                    ...(typeof window !== 'undefined' && window.matchMedia('(max-width: 580px)').matches
+                                        ? {
+                                            fontSize: '2em',
+                                            lineHeight: '35px',
+                                        }
+                                        : {}),
+
+                                }}> Google Developer Student Club</h2>
+                            <p
+                                className={Work.className}
+                                style={{
+                                    fontSize: '1.3em',
+                                    fontWeight: '400',
+                                    lineHeight: '25px',
+                                    letterSpacing: '0em',
+                                    marginTop: '15px',
+                                    textAlign: 'left',
+                                    color: '#787878',
+                                    ...(typeof window !== 'undefined' && window.matchMedia('(max-width: 580px)').matches
+                                        ? {
+                                            fontSize: '1em',
+                                            lineHeight: '20px',
+                                        }
+                                        : {}),
+                                }}>GDSC VIT Chennai is a student run community group powered by Google Developers. We are a passionate group of budding developers who work together to build solutions for local businesses and communities with Google technology and promote healthy developer culture.</p>
+                        </div >
+                    </div>
                 </div>
-            </section>
+            </section >
         </>
     );
 };
