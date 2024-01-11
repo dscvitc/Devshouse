1d151e59ba9ab586cc9f6f2188c07560276cb839
Ayush Awasthi <ayush.awasthi468@gmail.com>
2024-01-11 21:48:14 +0530

    Add Sponsor Us section, insert custom backgrounds and increase FAQ width on small devices

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,13 +1,16 @@
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import Image from "next/image";
 import FAQData from "./FAQData";
+import { Button } from '@mantine/core';
 import { Space_Grotesk } from 'next/font/google';
 import RedBubble from "../../public/green bubbles.svg";
 import YellowBubble from "../../public/green bubbles (1).svg";
 import BlueBubble from "../../public/green bubbles (2).svg";
 import GreenBubble from "../../public/green bubbles (3).svg";
+import SponsorDecor from "../../public/Group 43.svg"
 
 const Space = Space_Grotesk({ weight: "700", subsets: ["latin"] });
+const Space400 = Space_Grotesk({ weight: "400", subsets: ["latin"] });
 
 const FAQ = () => {
   const colorClasses = {
@@ -27,13 +30,13 @@ const FAQ = () => {
       <AccordionItem className="rounded-xl border-bottom bg-[#18191b]" value={props.question}>
         <AccordionTrigger className={`text-white text-left hover:bg-inherit`}>
           <div className={`flex  ${Space.className} md:text-2xl text-[0.8rem] items-center`}>
-          <Image
-            src={imageClasses[props.color as keyof typeof imageClasses]}
-            alt="bubbles"
-            className="border-bottom rounded-xl scale-75 origin-bottom-left"
+            <Image
+              src={imageClasses[props.color as keyof typeof imageClasses]}
+              alt="bubbles"
+              className="border-bottom rounded-xl scale-75 origin-bottom-left"
             />
-          {props.question}
-            </div>
+            {props.question}
+          </div>
         </AccordionTrigger>
         <AccordionContent className={`${colorClasses[props.color as keyof typeof colorClasses]} rounded-xl p-5`}>
           <p className={`${Space.className} pl-2 md:text-lg text-[0.8rem] text-black`}>{props.answer}</p>
@@ -43,14 +46,35 @@ const FAQ = () => {
   ));
 
   return (
-    <div id="faqs" className="flex justify-center items-center bg-bg-black pt-10">
-      <div className="bg-black md:w-[80%] h-auto">
-      <h1 className={`text-5xl ${Space.className} text-white text-center mb-14`}>FAQs</h1>
+    <>
+      <div id="faqs" className="flex justify-center bg-[url('../../public/bg-FAQ.svg')] items-center bg-bg-black pb-20 pt-10">
+        <div className="w-[90%] md:w-[80%] h-auto">
+          <h1 className={`text-5xl ${Space.className} text-white text-center mb-14`}>FAQs</h1>
           <Accordion type="single" className="space-y-4">
             {items}
           </Accordion>
+        </div>
       </div>
-    </div>
+      <div className={`bg-[url('../../public/bg-Sponsor-Us.svg')] p-14 pl-32 space-y-8 text-white text-left ${Space.className}`} id='sponsor-us'>
+        <div className="flex">
+        <h1 className="text-5xl" >Let's Host Together</h1>
+        <Image
+        src={SponsorDecor}
+        alt="Sponsor Heading Decoration"
+        className="-translate-y-7"
+        />
+        </div>
+        <div className={`flex text-3xl gap-12 ${Space400.className}`}>
+          <div className="pr-6">
+          <p>Elevate your brand, be part of the tech revolution!</p>
+          <p>Write to <b>collaborate</b> with us at: sponsorship@devshouse.tech</p>
+          </div>
+          <Button size="xl" className="button-Sponsor">
+             Sponsor Us
+          </Button>
+        </div>
+      </div>
+    </>
   );
 };
 
