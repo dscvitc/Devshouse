5a59d66bf1ebd5560246b44ae892ae67af7d13ce
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2024-01-13 23:22:56 +0530

    fixed some background issues

--- src/components/prizes/MainPrizeCard.tsx
+++ src/components/prizes/MainPrizeCard.tsx
@@ -1,25 +1,20 @@
-import Image from 'next/image'
+import Image from "next/image";
 type MainPrizeProp = {
-    src: string// image src link
-    width: number
-    height: number
-    price: string // Ex: 30,000
-    alt: string //1st, 2nd or 3rd
-}
+  src: string; // image src link
+  width: number;
+  height: number;
+  price: string; // Ex: 30,000
+  alt: string; //1st, 2nd or 3rd
+};
 
 export default function MainPrizeCard({ src, width, height, price, alt }: MainPrizeProp) {
-    return (
-        <div className='flex flex-col justify-end'>
-            <Image
-                src={src}
-                alt={`${alt} place`}
-                width={width}
-                height={height}
-                className="shadow"
-            />
-            <div className="text-center text-white text-[16px] font-bold font-['Space Grotesk'] md:text-[24px]  lg:text-[32px]">
-                <h1 >INR {price}</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-end">
+      <Image src={src} alt={`${alt} place`} width={width} height={height} className="shadow" />
+      <div className="text-center text-white text-[16px] font-bold font-['Space Grotesk'] md:text-[24px]  lg:text-[32px]">
+        {/* <h1 >INR {price}</h1> */}
+        <h1>{price}</h1>
+      </div>
+    </div>
+  );
 }
