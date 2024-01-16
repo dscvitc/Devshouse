4c0b7f82fcced7081a312a90a1ac3d69b9504f66
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2024-01-16 08:01:20 +0530

    footer changes

--- src/components/Footer2.tsx
+++ src/components/Footer2.tsx
@@ -4,14 +4,17 @@ import devshouseTicket from "../../public/assets/images/devshouseticket.svg";
 
 import { IconBrandDiscordFilled } from "@tabler/icons-react";
 import { IconBrandGithubFilled } from "@tabler/icons-react";
+import { IconBrandInstagram } from "@tabler/icons-react";
+import { IconBrandLinkedin } from "@tabler/icons-react";
+import { IconBrandX } from "@tabler/icons-react";
 
 export default function Footer2() {
   return (
-    <div className="w-full p-12 font-spaceGrotesk grid grid-cols-1 md:grid-cols-3 gap-12">
+    <div className="w-full p-12 font-spaceGrotesk gap-4 grid grid-cols-1 md:grid-cols-4 justify-center  ">
       <Image src={devshouseTicket} alt="Devshouse Ticket" className="self-center"></Image>
-      <div className="flex flex-col gap-4 justify-start">
+      <div className="flex flex-col gap-4 justify-start px-12 m-2">
         <div>
-          <p className="text-2xl font-bold">QuickLinks</p>
+          <p className="text-2xl font-bold">Quick Links</p>
         </div>
         <div className="flex flex-col">
           <a className="text-lg" href="#about">
@@ -37,39 +40,37 @@ export default function Footer2() {
           </a>
         </div>
       </div>
-      <div className="flex flex-col gap-4 justify-start">
+      <div className="flex flex-col gap-4 justify-start px-12 m-2">
         <div>
           <p className="text-2xl font-bold">Join us</p>
         </div>
-        <div className="flex flex-col">
-          <a href="https://github.com/dscvitc/dscvitchennai" className="text-xl">
+        <div className="flex flex-col gap-2">
+          <a href="https://github.com/dscvitc/dscvitchennai" target="_blank" className="text-xl">
             <span className="flex flex-row px-2 gap-4">
               <IconBrandGithubFilled /> <p>Github</p>
             </span>
           </a>
-          <a href="" className="text-xl">
+          <a href="https://discord.com/invite/UFfPjTtzh7" target="_blank" className="text-xl">
             <span className="flex flex-row px-2 gap-4">
               <IconBrandDiscordFilled /> <p>Discord</p>
             </span>
           </a>
         </div>
       </div>
-      <div className="flex flex-col gap-4 justify-start">
-        <div>
-          <p className="text-2xl font-bold">Join us</p>
-        </div>
-        <div className="flex flex-col">
-          <a href="https://github.com/dscvitc/dscvitchennai" className="text-xl">
-            <span className="flex flex-row px-2 gap-4">
-              <IconBrandGithubFilled /> <p>Github</p>
-            </span>
+      <div className="flex flex-col gap-4 justify-start px-12">
+        <p className="text-2xl font-bold">Follow us</p>
+        <p className="text-xl text-gray-500">Connect. Learn. Grow.</p>
+        <span className="flex flex-row py-2 gap-4 m-2">
+          <a href="https://www.instagram.com/devshouse.tech" target="_blank" className="hover:scale-125">
+            <IconBrandInstagram />
           </a>
-          <a href="" className="text-xl">
-            <span className="flex flex-row px-2 gap-4">
-              <IconBrandDiscordFilled /> <p>Discord</p>
-            </span>
+          <a href="https://www.linkedin.com/company/devshouse/" target="_blank" className="hover:scale-125">
+            <IconBrandLinkedin />
           </a>
-        </div>
+          <a href="https://twitter.com/devshouse_tech" target="_blank" className="hover:scale-125">
+            <IconBrandX />
+          </a>
+        </span>
       </div>
     </div>
   );
--- src/components/TimelineOnline.tsx
+++ src/components/TimelineOnline.tsx
@@ -6,7 +6,7 @@ export default function TimelineOnline() {
     <div className="flex flex-col items-center justify-center w-full my-8">
       <h1 className="text-white text-center text-5xl font-bold px-10">Timeline</h1>
       <div className="flex h-1/2">
-        <Image src={timelineonline} alt="profile" objectFit="cover" fill={false} className="w-full" />
+        <Image src={timelineonline} alt="timeline" objectFit="cover" fill={false} className="w-full" />
       </div>
     </div>
   );
