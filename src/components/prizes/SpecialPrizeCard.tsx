455976fca79c8d93d1d6d195bee9d5a3d2646632
AdityaS8804 <aditya.s2022a@vitstudent.ac.in>
2024-02-04 20:13:09 +0530

    Special track prizes updated

--- src/components/prizes/SpecialPrizeCard.tsx
+++ src/components/prizes/SpecialPrizeCard.tsx
@@ -9,7 +9,7 @@ type trackProp = {
 export default function SpecialPrizeCard({ trackName, trackLogo, trackText, color }: trackProp) {
     return (
         <div className="w-screen flex flex-row justify-center justify-self-center text-center items-center lg:w-screen">
-            <div className="w-[300px] h-[190px] mr-[-290px] backdrop-opacity-100 bg-neutral-200 rounded-[10.61px] flex flex-col justify-center  lg:w-[450px] lg:mr-[-440px] p-1 xl:w-[560px] xl:mr-[-547px]" >
+            <div className="w-[300px] h-[340px] mr-[-290px] backdrop-opacity-100 bg-neutral-200 rounded-[10.61px] flex flex-col justify-center  lg:w-[450px] lg:mr-[-440px] p-1 xl:w-[560px] xl:mr-[-547px]" >
                 <div className="flex flex-row justify-center items-center">
                     <Image
                         src={trackLogo}
@@ -21,7 +21,7 @@ export default function SpecialPrizeCard({ trackName, trackLogo, trackText, colo
                 </div>
                 <div className="w-full max-h-[75.26px] mt-3 text-center text-black text-[16px] font-bold font-['Product Sans'] xl:text-[21px]">{trackText}</div>
             </div>
-            <div className="w-[300px] h-[190px] rounded-[10.61px] border-2 border-white lg:w-[450px] xl:w-[560px] mt-[24px]" />
+            <div className="w-[300px] h-[340px] rounded-[10.61px] border-2 border-white lg:w-[450px] xl:w-[560px] mt-[24px]" />
         </div>
     );
 }
